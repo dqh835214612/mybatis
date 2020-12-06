bc1aae9292493caa8556f55922fada8f2d0eea1f
86184 <835214612@qq.com>
2020-12-06 21:54:55 +0800

    M

--- src/main/java/com/deng/mybatis/mapper/UserMapper.java
+++ src/main/java/com/deng/mybatis/mapper/UserMapper.java
@@ -2,7 +2,12 @@ package com.deng.mybatis.mapper;
 
 import com.deng.mybatis.entity.User;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserMapper  {
 
     User getUserById (Long id);
+
+    List<User> getUserAll();
 }
--- src/test/java/com/deng/mybatis/MybatisApplicationTests.java
+++ src/test/java/com/deng/mybatis/MybatisApplicationTests.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 class MybatisApplicationTests {
@@ -26,9 +29,41 @@ class MybatisApplicationTests {
     }
 
     @Test
+    void getUserAll() {
+
+        List<User> list = mapper.getUserAll();
+        Map<String, List<Object>> map = new HashMap<>();
+        System.err.println(list);
+        for (User user : list) {
+// map是否包含此key，若已经包含则添加一个新的数字到对应value集合中
+            if (map.containsKey(user.getName())) {
+                map.get(user.getName()).add(user.getAge());
+            } else {
+                List<Object> newList = new ArrayList<>();
+                newList.add(user.getAge());
+                map.put(user.getName(), newList);
+            }
+        }
+        System.out.println(map);
+
+        for (Map.Entry<String, List<Object>> entry : map.entrySet()) {
+                System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
+               }
+    }
+
+    @Test
     void contextLoads1() {
         User userById = userService.getUserById(1L);
         System.out.println(userById);
+    }
+    @Test
+    void getDate() {
+        List<User> list = mapper.getUserAll();
+        Set<String> set=new HashSet();
+        for (User user:list) {
+            set.add(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(user.getCreateTime()));
+        }
+        System.out.println(set);
     }
 
 }
